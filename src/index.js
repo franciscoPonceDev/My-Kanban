da760077a5d7c6ca971a63c079f6019a2e91375b
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-03-11 23:46:42 -0300

    local storage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.css';
 
 const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
 const saveItemBtns = document.querySelectorAll('.solid');
@@ -12,7 +12,7 @@ const completeList = document.getElementById('complete-list');
 const onHoldList = document.getElementById('on-hold-list');
 
 // Items
-
+const updatedOnLoad = false;
 
 // Initialize Arrays
 let backlogListArray = [];
@@ -21,9 +21,7 @@ let completeListArray = [];
 let onHoldListArray = [];
 let listArrays = [];
 
-
 // Drag Functionality
-
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -40,45 +38,54 @@ function getSavedColumns() {
   }
 }
 
-getSavedColumns();
-updateSavedColumns();
-
 // Set localStorage Arrays
 function updateSavedColumns() {
-    listArrays = [backlogListArray, progressListArray, completeListArray, onHoldListArray];
+  listArrays = [backlogListArray, progressListArray, completeListArray, onHoldListArray];
 
-    const arrayNames = ['backlog','progress', 'complete', 'onHold'];
-    arrayNames.forEach((arrayName, index) => {
-        localStorage.setItem(`${arrayName}Items`, JSON.stringify(listArrays[index]));
-    });
+  const arrayNames = ['backlog', 'progress', 'complete', 'onHold'];
+  arrayNames.forEach((arrayName, index) => {
+    localStorage.setItem(`${arrayName}Items`, JSON.stringify(listArrays[index]));
+  });
 }
+updateSavedColumns();
 
 // Create DOM Elements for each list item
 function createItemEl(columnEl, column, item, index) {
-  console.log('columnEl:', columnEl);
-  console.log('column:', column);
-  console.log('item:', item);
-  console.log('index:', index);
   // List Item
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
-
+  listEl.textContent = item;
+  columnEl.appendChild(listEl)
 }
 
 // Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
 function updateDOM() {
   // Check localStorage once
-
+  if (!updatedOnLoad) {
+    getSavedColumns();
+  }
   // Backlog Column
-
+  backlogList.textContent = '';
+  backlogListArray.forEach((backlogItem, index) => {
+    createItemEl(backlogList, 0, backlogItem, index);
+  });
   // Progress Column
-
+  progressList.textContent = '';
+  progressListArray.forEach((progressItem, index) => {
+    createItemEl(progressList, 0, progressItem, index);
+  });
   // Complete Column
-
+  completeList.textContent = '';
+  completeListArray.forEach((completeItem, index) => {
+    createItemEl(completeList, 0, completeItem, index);
+  });
   // On Hold Column
-
+  onHoldList.textContent = '';
+  onHoldListArray.forEach((onHoldItem, index) => {
+    createItemEl(onHoldList, 0, onHoldItem, index);
+  });
   // Run getSavedColumns only once, Update Local Storage
-
-
 }
 
+// OnLoad
+updateDOM();
